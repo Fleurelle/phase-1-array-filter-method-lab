0340a491f869ec109e6de846e62ab8a96db5b8c6
Rose <r.d.jeanlouis01@gmail.com>
2021-04-21 20:22:56 -0400

    commit tree 870d25173138ac9f41bae2f4a9840ae804a01e12

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// findMatching function - returns all drivers that match the passed in name:
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name2){
+    return drivers.filter(function (el){
+        const itemsList = el.toLowerCase() === name2.toLowerCase();
+    return itemsList;
+    })
+}
+
+function fuzzyMatch(drivers, name3){
+    return drivers.filter(function (element){
+        return element.toLowerCase().indexOf(name3.toLowerCase()) === 0;
+    })
+}
+
+function matchName(drivers, name4){
+    return drivers.filter(function (filterMatch){
+        return filterMatch["name"] === name4;
+    } )
+
+}
